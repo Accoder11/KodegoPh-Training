35d0a908c49cc86b092b10c02ca09b06f0160496
Alma Cleto <emscleto11@gmail.com>
2022-12-12 09:42:21 +0800

    created simple calculator

--- 12122022/calculator.js
+++ 12122022/calculator.js
@@ -8,17 +8,21 @@ const operator = prompt('Enter operator ( either +, -, * or / )');
 // Ask for the 2nd number
 const num2 = prompt ("Enter the 2nd number ");
 
-alert ("RESULT: " + Number(num1) + operator + Number(num2));
+alert ("You've entered: " + Number(num1) + " " + operator + " " + Number(num2));
 
 if (operator == '+') {
-    console.log ("Sum: " + (Number (num1) + Number (num2)));
+    let h1 = document.getElementById("result");
+    h1.innerHTML = ("Sum: " + (Number (num1) + Number (num2)));
 }
 else if (operator == '-') {
-    console.log ("Difference: " + (Number (num1) - Number (num2)));
+    let h1 = document.getElementById("result");
+    h1.innerHTML = ("Difference: " + (Number (num1) - Number (num2)));
 }
 else if (operator == '*') {
-    console.log ("Product: " + (Number (num1) * Number (num2)));
+    let h1 = document.getElementById("result");
+    h1.innerHTML = ("Product: " + (Number (num1) * Number (num2)));
 }
 else {
-    console.log ("Quotient: " + (Number (num1) / Number (num2)));
+    let h1 = document.getElementById("result");
+    h1.innerHTML = ("Quotient: " + (Number (num1) / Number (num2)));
 }
